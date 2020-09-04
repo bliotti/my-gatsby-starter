458c535897eb3b9216f0ad494c00af2a3735cc40
Brian Liotti <bliotti@protonmail.com>
2020-09-03 22:00:56 -0400

    jpeg

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,13 @@
 import React from 'react';
 import Page from '../components/Page';
+const TSimg = require('../assets/typescript-all-the-things.jpeg');
 
 const Home = () => {
-  return <Page>Hello, world!</Page>;
+  return (
+    <Page>
+      <img src={TSimg} />
+    </Page>
+  );
 };
 
 export default Home;
